4825f7d80c87d0b9744bf2f90d3f7beb84a1670e
HChaoHui <133383328+HChaoHui@users.noreply.github.com>
2024-01-13 20:56:44 +0800

    添加PandoraNext site_password的支持，添加注册池
    添加PandoraNext site_password的支持
    添加PandoraNext注册池（可以添加多个PandoraNext注册地址，没有额度时会自动切换）

--- util/config.js
+++ util/config.js
@@ -1,7 +1,9 @@
 let config = {
-    RegisteredAddress: "https://chat.oaifree.com/auth/signup", // 注册地址，默认使用zhile大佬的https://chat.oaifree.com/
+    RegisteredAddress: ["https://chat.oaifree.com/auth/signup"], // 注册地址，默认使用zhile大佬的https://chat.oaifree.com/，如果有多个以数组的形式存储，例如[xx.com,yy.com,zz.com]
+    RegisteredIndex: 0,
     MailAddress: "https://ihotmails.com/", // 注册邮箱地址，Google搜索Forsaken Mail临时邮箱，默认使用https://ihotmails.com/
-    headless:"new", // 是否开启浏览器无头模式，为false时为关闭，有问题时可以改为false做调试，查看问题原因
+    headless: "new", // 是否开启浏览器无头模式，为false时为关闭，有问题时可以改为false做调试，查看问题原因
+    sitePassword: "", // 如果PandoraNext设置了site_password，填写在这里，没有则为空
 }
 
 module.exports = config
